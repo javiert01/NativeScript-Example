426c45c05a799381127dbb4fce867d20c0565264
javiert01 <javotinoco@hotmail.com>
2019-04-17 00:16:42 -0500

    first commit, added navigation and dinamic routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit, ChangeDetectorRef } from "@angular/core";
+import { UIService } from "./shared/ui.service";
+import { Subscription } from "rxjs";
+import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular/side-drawer-directives";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+
+@Component({
+    selector: "ns-app",
+    moduleId: module.id,
+    templateUrl: "./app.component.html"
+})
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit{
+
+    @ViewChild(RadSideDrawerComponent) drawerComponent: RadSideDrawerComponent;
+    private drawer: RadSideDrawer;
+    activeChallenges: string[] = [];
+    private drawerSub: Subscription;
+    constructor(private uiService: UIService, private changeDetectionRef: ChangeDetectorRef){}
+
+    ngOnInit() {
+        this.drawerSub = this.uiService.drawerState.subscribe(
+            () => {
+                if(this.drawer){
+                    this.drawer.toggleDrawerState();
+                }
+            });
+    }
+
+    ngAfterViewInit() {
+        this.drawer = this.drawerComponent.sideDrawer;
+
+        this.changeDetectionRef.detectChanges();
+    }
+
+
+
+    onChallengeInput(challengeDescription: string){
+        this.activeChallenges.push(challengeDescription);
+    }
+
+    onLogout(){
+        this.uiService.toggleDrawer();
+    }
+
+    ngOnDestroy(){
+        if(this.drawerSub){
+            this.drawerSub.unsubscribe();
+        }
+    }
+}
---
+++ src/app/challenges/today/today.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-today',
+  templateUrl: './today.component.html',
+  styleUrls: ['./today.component.css'],
+  moduleId: module.id,
+})
+export class TodayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
